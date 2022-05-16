c9a5927a083a5be321f0832e326fc943aac9a9a3
Morad Zahid <morad.zahid@laposte.net>
2022-05-16 21:12:04 +0200

    change text about the author

--- view/authorFrenchTemplate.php
+++ view/authorFrenchTemplate.php
@@ -1,7 +1,14 @@
 <div id='author'>
 	<h3> A propos de l'auteur</h3>
-	<p> Titulaire d'un master de mathématiques, évoluant dans le monde de l'éducation,
-		je suis en reconversion dans les métiers du développement informatique.</p>
-	<p> Je vous invite à regarder les diagrammes UML du projet.</p>
+	<p> Titulaire d'un Master de Biophysique et d'un Master de Mathématiques j'ai 
+		évolué dans le monde de la recherche, à l'interface entre la physique et la 
+		neurobiologie, puis dans celui de la pédagogie avant de me spécialiser dans les 
+		métiers du développement web.
+	</p>
+	<p>
+		Actuellement je suis la formation professionnelle "Développeur web et web mobile"
+		 dispensée par l'Afpa. Cette formation me prépare à développer des applications 
+		 web dynamiques, adaptables et sécurisées.
+	</p>
 	<p>Si vous souhaitez me contacter, vous pouvez m'envoyer un <a href="mailto:morad.zahid@laposte.net">e-mail</a>.</p>
 </div>
